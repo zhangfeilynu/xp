09eed2e6257ac4e6e4ff15c30f26e4ec454a9401
unknown <fei.zhang@molbase.com>
2016-11-25 11:36:02 +0800

    111

---
+++ src/main/java/com/zf/selenium2/xp/App.java
@@ -0,0 +1,13 @@
+package com.zf.selenium2.xp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
